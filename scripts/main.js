58c6eca4984a39c9d87381e44cf2f4b171fe828f
SlavicC1 <v.shinckaruck@yandex.ru>
2019-05-09 21:49:46 +0300

    Add todo list. Almost all done!

--- scripts/main.js
+++ scripts/main.js
@@ -18,4 +18,61 @@ var timer = (function (){
     return this;
 })();
 
-setInterval(function(){ timer.Update(); timer.SetTimer();},100);
+var toDoList = (function(){
+    var elements = [];
+    function Element(name,text){
+        this.name = name;
+        this.text = text;
+        return this;
+    }
+    this.AddElement = function(name,text){
+        elements.push(new Element(name,text));
+    }
+    this.Update = function(){
+        
+    }
+    var removeElement = function(nom){
+        setTimeout(function(){
+            elements.splice(nom);
+            Show();
+        },500);
+    }
+    this.Show = function(){
+        var listElem = document.getElementById("to-do-list");
+        clearList(listElem);
+        for(var i = 0; i < elements.length; i++){
+            showSingleElement(listElem,elements[i]);
+        }
+    }
+    function showSingleElement(listElem, element){
+        var elem = document.createElement("div");
+        elem.classList.add("b-to-do-list__to-do_w500");
+        elem.innerHTML = element.name + "<br>" + element.text;
+        var checkELem = document.createElement("div");
+        checkELem.innerHTML = "<input type=\"checkbox\">"
+        checkELem.classList.add("b-to-do-list__check");
+        var parentElem = document.createElement("div");
+        parentElem.classList.add("b-to-do-list__to-do");
+        parentElem.appendChild(checkELem);
+        parentElem.appendChild(elem);
+        listElem.appendChild(parentElem);
+    }
+    function clearList(listElem){
+        while(listElem.firstChiled){
+            listElem.removeChiled(listElem.firstChiled);
+        }
+    }
+
+    return this;
+})();
+
+toDoList.AddElement("Написать \"тудушку\".","Заставить работать текущий проект всеми силами.");
+toDoList.AddElement("Написать \"тудушку\".","Заставить работать текущий проект всеми силами.");
+toDoList.AddElement("Написать \"тудушку\".","Заставить работать текущий проект всеми силами.");
+toDoList.AddElement("Написать \"тудушку\".","Заставить работать текущий проект всеми силами.");
+
+toDoList.Show();
+setInterval(function(){ 
+    timer.Update(); 
+    timer.SetTimer();
+},100);
